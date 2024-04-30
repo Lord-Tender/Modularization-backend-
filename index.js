cd5d1a3fc800fa7f7428ed57b13fcdb38f1a721d
Emmanuel <emmanuelola961@gmail.com>
2024-04-30 12:58:05 +0100

    pushing backend 4

--- index.js
+++ index.js
@@ -6,9 +6,16 @@ let uri = process.env.URI
 let cors = require('cors')
 const mongoose = require('mongoose')
 const userRouter = require("./Routes/User.Route")
+const allowedOrigins = [`http://localhost:${port}`, 'https://your-production-app.com']
+const corsOptions = {
+    origin: allowedOrigins,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'], // Adjust as needed
+  };
+
 app.use(express.urlencoded( {extended:true, limit: "50mb"} ))
 
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(express.json( { limit: "50mb" } ))
 app.use('/', userRouter)
 
